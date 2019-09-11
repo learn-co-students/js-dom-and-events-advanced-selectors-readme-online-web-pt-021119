b8b2a3de7d562d7859cf33bce8966256a52ee496
Ryan Wolfe <ryan.d.wolfe@gmail.com>
2019-09-11 11:50:57 -0500

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  const example = $('#pic-list li:first-child');
+  return example
+}
